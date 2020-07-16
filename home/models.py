410db03c0729b0482411902d35de6583b797263f
Ayushi Verma <yuktaverma2005@gmail.com>
2020-07-16 17:36:13 +0530

    modification and migrations-2

--- home/models.py
+++ home/models.py
@@ -7,3 +7,4 @@ class Contact(models.Model):
     phone=models.CharField(max_length=13)
     email=models.CharField(max_length=70)
     content=models.TextField
+    timeStamp=models.DateTimeField(auto_now_add=True,blank=True)
