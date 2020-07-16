54ea468b6ecc14aa4c95c397771f52168f67d37b
Ayushi Verma <yuktaverma2005@gmail.com>
2020-07-16 12:26:35 +0530

    Changes in Blog app

--- Blog/views.py
+++ Blog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+def blogHome(request):
+    return HttpResponse('This is blogHome. we will keep all blog post here')
+def blogPost(request,slug):
+    return HttpResponse(f'This is blogPost:{slug}')
