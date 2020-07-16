a57bee6c5f378b3d7511d545fb3129cfd7a67d13
Ayushi Verma <yuktaverma2005@gmail.com>
2020-07-16 12:24:32 +0530

    Home apps

---
+++ home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.def home(index):
+def home(request):
+    return HttpResponse('This is home.')
+def about(request):
+    return HttpResponse('This is about.')
+def contact(request):
+    return HttpResponse("This is contact")
