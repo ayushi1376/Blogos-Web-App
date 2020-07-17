5b8ff9f9c8c112811f98e15ad9de46f5db1ef623
Ayushi Verma <yuktaverma2005@gmail.com>
2020-07-17 14:35:06 +0530

    Blog display changes

--- Blog/views.py
+++ Blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render,HttpResponse
+from blog.models import Post
+
 
 # Create your views here.
 def blogHome(request):
-    return render(request,'blog/blogHome.html')
-    # return HttpResponse('This is blogHome. we will keep all blog post here')
+    allPosts=Post.objects.all()
+    context={'allPosts':allPosts}
+    return render(request,'blog/blogHome.html',context)
+    
+
 def blogPost(request,slug):
     return render(request,'blog/blogPost.html')
-    # return HttpResponse(f'This is blogPost:{slug}')
+    
