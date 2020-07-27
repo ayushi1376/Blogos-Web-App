3aa337e39286613f05f6eaa47117ffe2de0e7cae
Ayushi Verma <yuktaverma2005@gmail.com>
2020-07-27 20:37:20 +0530

    Comments

--- Blog/views.py
+++ Blog/views.py
@@ -13,7 +13,8 @@ def blogHome(request):
 def blogPost(request,slug):
     post=Post.objects.filter(slug=slug).first()
     comments=Blogcomment.objects.filter(post=post)
-    context={'post':post,'comments':comments}
+    print(request.user)
+    context={'post':post,'comments':comments, 'user': request.user}
     return render(request,'blog/blogPost.html',context)
     
 
